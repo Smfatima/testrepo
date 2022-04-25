009dab068d27e47a372c39d3d1d19ed245330a53
S.M.Fatima <s.m.fatima13@gmail.com>
2022-04-25 16:55:27 +0500

    Add files via upload

---
+++ sample.sh
@@ -0,0 +1,6 @@
+#!bin/bash/
+echo "enter value for A"
+read a
+echo "enter value for B"
+read b 
+python samplerun.py $a $b
---
+++ samplerun.py
@@ -0,0 +1,5 @@
+import sys
+
+input = sys.argv
+
+print (int(input[1])+int(input[2]))
